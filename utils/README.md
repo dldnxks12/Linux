d6deec39b09e2667f01bb51165e3f77e63e95fc3
JS <dldnxks12@naver.com>
2021-08-12 22:04:02 +0900

    0802

--- utils/README.md
+++ utils/README.md
@@ -1,4 +1,4 @@
-#### command line arguments 
+#### Command line arguments 
 
 	int main(int argc, char* argv)에 대한 이해가 어느 정도 필요
 
@@ -10,3 +10,37 @@
 ![KakaoTalk_20210803_102829756](https://user-images.githubusercontent.com/59076451/127943423-9f7a5629-4f71-4a43-baf1-016c09d3ec62.jpg)
 
 ![111](https://user-images.githubusercontent.com/59076451/127943334-384b000e-9290-4d26-b451-09883c8a9b60.PNG)
+
+
+#### exit(0), exit(1), return ?
+
+	exit(0) : EXIT_SUCCESS - 정상 종료
+
+	exit(1) : EXIT_FAILURE - 에러 메세지 종료
+
+	return  : exit()과는 다르게 뒤의 문장을 수행한 후에 종료한다.
+
+		일반 함수 내에서 return 사용시 해당 함수 종료 
+	
+		main() 함수 내에서 return 사용시 해당 프로세스를 종료
+
+
+#### Option Parsing in C
+
+
+	Linux의 대부분 명령어는 - 로 시작하는 실행 인자로 옵션을 지정할 수 있다!
+
+		ex) ls -l , ls -al , ...
+
+	옵션은 해당 명령어를 우리가 만들 때 정의한 것으로, Kernel or OS가 그 형태를 강제하지 않는다.
+
+	추가적으로 '-' , '--' 인자가 다음의 의미를 갖는다.
+
+		'-'  : 표준 입력에서 읽으라
+		'--' : 이 이후의 인자는 옵션으로 해석되지 않는다. 
+
+			-로 시작하는 문자열을 인자로 전달하고 싶을 때 사용한다.
+
+
+
+	자세한 내용 : head_parser.c code 참조
--- utils/head.c
+++ utils/head.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  // header for standard IO
+#include <stdlib.h> // header for exit
 
+// File pointer, nlines : 읽을 줄 수
 static void do_head(FILE* f, long nlines);
 
-// 첫 번째 인자는 받아들인 줄 수, 두 번째 인자는 파일 이름
+
 
 int main(int argc, char* argv[])
 {
@@ -20,17 +21,17 @@ int main(int argc, char* argv[])
 
 	if (argc == 2) // file 인자가 없을 경우, stdin의 내용을 출력
 
-		do_head(stdin, atol(argv[1]));
+		do_head(stdin, atol(argv[1])); // 표준 입력으로 넣어준 것을 바로 출력
 
-	// 해당 file의 내용
+	// file 인자가 있을 경우에는 해당 file의 내용을 출력
 	else
 	{
-		int i;
+		int i; // file 인자 개수에 맞게
 
-		for(i=0; i<argc; i++)
+		for(i=0; i < argc; i++)
 		{
-			FILE* f;
-			f = fopen(argv[i], "r");
+			FILE* f; // FILE 구조체 포인터 생성
+			f = fopen(argv[i], "r"); // 해당 file로 Stream 생성
 
 			if(!f) // f == NULL 과 동일한 문장
 			{
@@ -39,7 +40,8 @@ int main(int argc, char* argv[])
 			}
 
 			do_head(f, nlines);
-			fclose(f);
+
+			fclose(f); // FILE 구조체에 포함된 stream 닫기
 		}
 	}
 
@@ -50,16 +52,17 @@ int main(int argc, char* argv[])
 
 static void do_head(FILE* f, long nlines)
 {
-
 	int c;
 
-	while((c = fgetc(f)) != EOF)  // fgets를 사용하지 않는 이유? --- buffer를 만들어서 넘길 필요가 없다
+	// fgetc(FILE* stream) -- stream에서 1 byte 씩 가져오기
+	while( (c = fgetc(f)) != EOF )  // fgets를 사용하지 않는 이유? --- buffer를 만들어서 넘길 필요가 없다
 	{
-		if(putchar(c) < 0) // putchar == fputc(c, stdout)
+		// 읽은 1 byte를 stdout으로 보내기
+		if(putchar(c) < 0) // putchar == fputc(int c, FILE* stream)   --- fputc(c, stdout)
 		{
 			exit(1);
 		}
-		if(c == '\n')
+		if(c == '\n') // 개행 문자 만나면 줄 - 1
 		{
 			nlines--;
 		}
@@ -67,7 +70,5 @@ static void do_head(FILE* f, long nlines)
 		{
 			return 0;
 		}
-
 	}
-
 }
